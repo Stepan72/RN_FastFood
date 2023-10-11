2d0e2b87574ac943dd79440b6a18183af8dd7af8
Stepan72 <st.nim@list.ru>
2023-10-11 06:49:58 +0600

    FastFood app started v0.01

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 relative">
+      <Image
+        blurRadius={40}
+        source={require("../assets/images/background.png")}
+        className="flex-1 absolute w-full h-full"
+      />
+    </View>
+  );
+}
